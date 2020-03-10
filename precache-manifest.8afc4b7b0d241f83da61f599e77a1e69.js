1518ec7fffe76c254b72f67f6a49a2d604226a6d
Charles Gallay <charles.gallay@epfl.ch>
2020-03-10 08:30:21 +0100

    Updates

---
+++ precache-manifest.8afc4b7b0d241f83da61f599e77a1e69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717305cdf1b30522a9e6acf4ba2b8719",
+    "url": "/BrainViewer/index.html"
+  },
+  {
+    "revision": "427bb3bae13ae16c03c6",
+    "url": "/BrainViewer/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "74f8e16ea67d4b159d44",
+    "url": "/BrainViewer/static/css/main.1e1c5b9b.chunk.css"
+  },
+  {
+    "revision": "427bb3bae13ae16c03c6",
+    "url": "/BrainViewer/static/js/2.1ea872a0.chunk.js"
+  },
+  {
+    "revision": "22c76521cdd09224644a50323def27cc",
+    "url": "/BrainViewer/static/js/2.1ea872a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f8e16ea67d4b159d44",
+    "url": "/BrainViewer/static/js/main.579828eb.chunk.js"
+  },
+  {
+    "revision": "896565a4dd143b03b054",
+    "url": "/BrainViewer/static/js/runtime-main.0221dea0.js"
+  },
+  {
+    "revision": "bcb183bd001f13d71897ea6468c9ab82",
+    "url": "/BrainViewer/static/media/brain_icon.bcb183bd.svg"
+  }
+]);
